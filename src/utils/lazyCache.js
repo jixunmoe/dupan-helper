e56b8f4064c3af7f203ec56fd2abff6bd60f361b
Jixun Wu <jixun.moe@gmail.com>
2021-06-22 01:51:46 +0100

    fix eslint warning

--- src/utils/lazyCache.js
+++ src/utils/lazyCache.js
@@ -1,13 +1,13 @@
 const cache = (value) => () => value;
 
 export default function lazyCache(fn) {
-  let cacheWrapper = function () {
+  let cacheWrapper = function uncached() {
     const result = fn.apply(this, arguments);
     cacheWrapper = cache(result);
     return result;
   };
 
-  return function () {
+  return function cacheProxy() {
     return cacheWrapper.apply(this, arguments);
   };
 }
